5f93c0c614e26c60d952d66108bb1bc1be4a1253
Samier Saeed <samier_saeed@alumni.brown.edu>
2016-10-19 16:04:39 -0700

    hacking

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,53 @@
 import Koa from "koa";
 
 type middleware = (ctx: Koa.Context, next: Function) => Promise<void>;
-type middlwareFactory = (options: any) => middleware;
+type koaControllerAction = (ctx: Koa.Context) => Promise<any>;
 
-function toDecorator(mw: middlewareFactory) : (middleware) => void {
-    
-    return function(target: middleware, key?: any, descriptor?: any): void {
-        console.log(`${target.name} key: ${key}`);
-        console.log(descriptor);
+function toDecorator(middleware)  {
+    return function(target: any, key: string | symbol, descriptor: any): void {
+        let action: koaControllerAction = target[key];
+        let decorated = async (ctx: Koa.Context) => {
+            await middleware(ctx, action);
+        }
+        
+        descriptor.value = decorated;
     } 
-    
 }
 
 
-function testMiddleware(options: any){
+function testMiddleware1(options: any){
     return async (ctx: Koa.Context, next: Function) => {
         try {
+            console.log(options.foo);
             await next();
+            console.log('sdfdsfs!!!');
         }
-        
+
         catch (err){
             console.error(err);
         }
     }
 }
 
+const mockContext = <Koa.Context>{};
 
-toDecorator(testMiddleware);
+const testDecorator1 = toDecorator(testMiddleware1({foo: true}));
+
+class FakeController {
+    @testDecorator1
+    async testEndpoint(ctx: Koa.Context): Promise<void> {
+        console.log('oboy');
+    }
+}
+
+
+async function test(){
+   const f = new FakeController();
+   f.testEndpoint(mockContext); 
+}
+
+test();
+
+
+
+
