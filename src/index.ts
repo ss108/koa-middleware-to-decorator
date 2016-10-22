8f7f301d2a4b4c74c623e0e7ac75fb03cd657a4b
Samier Saeed <samier_saeed@alumni.brown.edu>
2016-10-21 18:05:19 -0700

    cleaning

--- src/index.ts
+++ src/index.ts
@@ -3,65 +3,60 @@ import Koa from "koa";
 type middleware = (ctx: Koa.Context, next: Function) => Promise<void>;
 type koaControllerAction = (ctx: Koa.Context) => Promise<any>;
 
-function toDecorator(middleware)  {
+export function toDecorator(middleware)  {
     return function(target: any, key: string | symbol, descriptor: any): void {
         let action: koaControllerAction = descriptor.value; 
-        // console.log(action);
+
         let decorated = async (ctx: Koa.Context) => {
             await middleware(ctx, action);
         }
         
         descriptor.value = decorated;
-        descriptor.decoratorCount = descriptor.decoratorCount || 0;
-        descriptor.decoratorCount += 1;
-
-        // console.log(descriptor.decoratorCount);
-        // console.log(descriptor.value);
     } 
 }
 
 
-function testMiddleware(options: any){
-    return async (ctx: Koa.Context, next: Function) => {
-        try {
-            console.log(options.foo);
-            await next();
-            console.log('sdfdsfs!!!');
-        }
+// function testMiddleware(options: any){
+//     return async (ctx: Koa.Context, next: Function) => {
+//         try {
+//             console.log(options.foo);
+//             await next();
+//             console.log('sdfdsfs!!!');
+//         }
 
-        catch (err){
-            console.error(err);
-        }
-    }
-}
+//         catch (err){
+//             console.error(err);
+//         }
+//     }
+// }
 
-function anotherOne(options?: any){
-    return async (ctx: Koa.Context, next: Function) => {
-        console.log("first");
-        await next();
-    }
-}
+// function anotherOne(options?: any){
+//     return async (ctx: Koa.Context, next: Function) => {
+//         console.log("first");
+//         await next();
+//     }
+// }
 
-const mockContext = <Koa.Context>{};
+// const mockContext = <Koa.Context>{};
 
-const testDecorator = toDecorator(testMiddleware({foo: true}));
-const anotherDecorator = toDecorator(anotherOne({}));
+// const testDecorator = toDecorator(testMiddleware({foo: true}));
+// const anotherDecorator = toDecorator(anotherOne({}));
 
-class FakeController {
-    @anotherDecorator
-    @testDecorator
-    async testEndpoint(ctx: Koa.Context): Promise<void> {
-        console.log('oboy');
-    }
-}
+// class FakeController {
+//     @anotherDecorator
+//     @testDecorator
+//     async testEndpoint(ctx: Koa.Context): Promise<void> {
+//         console.log('oboy');
+//     }
+// }
 
 
-async function test(){
-   const f = new FakeController();
-   f.testEndpoint(mockContext); 
-}
+// async function test(){
+//    const f = new FakeController();
+//    f.testEndpoint(mockContext); 
+// }
 
-test();
+// test();
 
 
 
