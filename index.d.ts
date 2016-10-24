915dae5dbb816bfa8aa40811d57ec277cf877320
Samier Saeed <samier_saeed@alumni.brown.edu>
2016-10-24 16:44:01 -0700

    fixed dec

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,6 @@ import * as Koa from "koa";
 import {middleware, koaControllerAction} from "./src";
 
 declare module "mw2dec" {
-    export function toDecorator(middleware: middleware) : void;
+    export function toDecorator(middleware: middleware) : (target: any, key: string | symbol, descriptor: any) => void;
 }
 
