7f1550c4d8bdbdfe165102061ed4da869879e53a
Samier Saeed <samier_saeed@alumni.brown.edu>
2016-10-28 17:04:02 -0700

    allow for decoration of classes

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as Koa from "koa";
-import { middleware, middlewareFactory, jsDecorator, decoratorFactory, koaControllerAction } from "./src";
-
-declare module "mw2dec" {
-    export function toOptionedDecorator(middleware: middleware): jsDecorator;
-    export function toDecorator(middlewareFactory): decoratorFactory;
-}
-
--- src/index.ts
+++ src/index.ts
@@ -6,41 +6,47 @@ export type koaControllerAction = (ctx: Koa.Context) => Promise<any>;
 export type jsDecorator = (target: any, key: string | symbol, descriptor: any) => void;
 export type decoratorFactory = (options: any) => jsDecorator;
 
-function _decorateClass(target){
-    let keys: PropertyKey[] = Reflect.ownKeys(target).filter((k) => {
-       return k !== "constructor"; 
-    });
-
+function _isClass(target: any): boolean {
+    return (typeof target !== "function");
 }
 
-function _decorateMethod(){
-    
+function _decorateClass(target: any, mw: middleware) {
+    let keys: PropertyKey[] = Reflect.ownKeys(target).filter((k) => {
+        return k !== "constructor";
+    });
+
+    keys.forEach((k) => {
+        let descriptor: PropertyDescriptor = Object.getOwnPropertyDescriptor(target, k);
+        _decorateMethod(mw, target, k, descriptor);
+    });
+}
+
+function _decorateMethod(mw: middleware, target: any, key: PropertyKey, descriptor: PropertyDescriptor) {
+    let action: koaControllerAction = descriptor.value;
+
+    let decorated = async (ctx: Koa.Context) => {
+        await mw(ctx, action.bind(null, ctx));
+    }
+
+    descriptor.value = decorated;
 }
 
 export function toOptionedDecorator(middleware): jsDecorator {
-    return function (target: any, key: string | symbol, descriptor: any): void {
-        let action: koaControllerAction = descriptor.value;
-
-        let decorated = async (ctx: Koa.Context) => {
-            await middleware(ctx, action.bind(null, ctx));
+    return function (target: any, key?: PropertyKey, descriptor?: PropertyDescriptor): void {
+        if (!key && !descriptor) {
+            _decorateClass(target, middleware);
         }
 
-        descriptor.value = decorated;
+        else {
+            _decorateMethod(middleware, target, key, descriptor);
+        }
     }
 }
 
 export function toDecorator(factory: middlewareFactory): decoratorFactory {
     return function (options: any) {
         let middleware = factory(options);
-        return function (target: any, key: string | symbol, descriptor: any): void {
-            let action: koaControllerAction = descriptor.value;
-
-            let decorated = async (ctx: Koa.Context) => {
-                await middleware(ctx, action.bind(null, ctx));
-            }
-
-            descriptor.value = decorated;
-        }
+        return toOptionedDecorator(middleware);
     }
 }
 
